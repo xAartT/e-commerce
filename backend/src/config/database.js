bc093339ad192fd6befe704a4c5f0a210c7154cc
xAartT <arturbresolin20@gmail.com>
2025-12-07 14:44:14 -0300

    rejectUnauthorized false

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' 
-    ? { rejectUnauthorized: false } 
-    : false,
+  ssl: {
+    rejectUnauthorized: false
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
